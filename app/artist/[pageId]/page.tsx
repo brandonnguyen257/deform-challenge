a1dc2a889540208af767da937ca4b52332732d8e
Brandon Nguyen <brandonnguyen257@gmail.com>
2024-02-16 20:16:19 -0800

    create test endpoint to use for artist page gating

--- app/artist/[pageId]/page.tsx
+++ app/artist/[pageId]/page.tsx
@@ -1,7 +1,49 @@
+'use client'
+import { getAccessToPage } from "@/services/wallet/WalletService";
+import { useEffect, useState } from "react";
+
+// const getAccessToPage = async () => {
+//     try {
+//       const res = await fetch('/wallet/access');
+//       return res.json();
+//     } catch (error) {
+//       console.error('Error:', error);
+//     }
+//   };
+
 export default function ArtistPage({
     params
 } : {
     params: {pageId: string}}) {
-        
-    return <h1>Hello Artist Page {params.pageId}</h1>
+
+      const [hasAccess, setHasAccess] = useState(false);
+
+      useEffect(() => {
+        const verifyAccess = async () => {
+          const res = await getAccessToPage();
+          setHasAccess(res.hasAccess);
+        };
+    
+        verifyAccess();
+      }, []); // Empty dependency array means this effect runs once on mount
+
+      if (!hasAccess) {
+        return <p>You do not have access to this page.</p>;
+      }
+
+    const onButtonClick = async () => {
+        // const res = await callApi();
+        console.log('onButtonClick');
+    }
+
+  
+
+
+    return (
+        <div>
+            <h1>Hello Artist Page {params.pageId}</h1>
+          <button onClick={onButtonClick}>Call API</button>
+          {/* {response && <p>{response.message}</p>} */}
+        </div>
+      );
 }
---
+++ services/wallet/WalletService.ts
@@ -0,0 +1,8 @@
+export const getAccessToPage = async () => {
+    try {
+      const res = await fetch('/wallet/access');
+      return res.json();
+    } catch (error) {
+      console.error('Error:', error);
+    }
+};
