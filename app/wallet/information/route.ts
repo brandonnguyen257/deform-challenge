245fe34a9a7a68f2b9f9da2d4150450c7cb29fe0
Brandon Nguyen <brandonnguyen257@gmail.com>
2024-02-16 22:07:52 -0800

    remove extra lines

--- app/wallet/information/route.ts
+++ app/wallet/information/route.ts
@@ -2,18 +2,5 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-
     return NextResponse.json({hasAccess: true});
-
 }
-
-
-// app.get('/personal_information', function (req, res) {
-//     if (!req.session.siwe) {
-//         res.status(401).json({ message: 'You have to first sign_in' });
-//         return;
-//     }
-//     console.log("User is authenticated!");
-//     res.setHeader('Content-Type', 'text/plain');
-//     res.send(`You are authenticated and your address is: ${req.session.siwe.address}`);
-// });
